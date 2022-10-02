69ecd622a7a22f847bf0835edc473462af1e5112
arcbjorn <arky.happ@gmail.com>
2022-10-02 20:27:06 +0400

    feat(BlogPostPage): use locales for dateFormatter

--- routes/[id].tsx
+++ routes/[id].tsx
@@ -1,21 +1,28 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
 import { CSS, render } from "$gfm";
-import { IPost } from "../types.ts";
+import { IPost, IState } from "../types.ts";
 import { loadPost } from "../api/loadPost.ts";
 
-export const handler: Handlers<IPost | false> = {
+interface IData extends IState {
+  post: IPost | null;
+}
+
+export const handler: Handlers<IData, IState> = {
   async GET(_req, ctx) {
     const id = ctx.params.id;
     const post = await loadPost(id);
     if (!post) {
-      return ctx.render(false);
+      return ctx.render({ ...ctx.state, post: null });
     }
-    return ctx.render(post);
+    return ctx.render({ ...ctx.state, post });
   },
 };
 
 export default function BlogPostPage(props: PageProps) {
-  const post = props.data;
+  const { post, locales } = props.data;
+
+  const dateFmt = new Intl.DateTimeFormat(locales, { dateStyle: "full" });
+
   const html = render(post.content);
 
   if (!props.data) {
@@ -25,7 +32,7 @@ export default function BlogPostPage(props: PageProps) {
   return (
     <div class="p-4 mx-auto max-w-screen-md">
       <p class="text-grey-600 mt-12">
-        {post.publishedAt.toLocaleDateString()}
+        {dateFmt.format(post.publishedAt)}
       </p>
       <h1 class="text-5xl mt-2 font-bold">{post.title}</h1>
       <style class="mt-12" dangerouslySetInnerHTML={{ __html: CSS }} />
