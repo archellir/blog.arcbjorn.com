07135124f86a35b3fec379dd358e1dee9e4687c9
arcbjorn <arky.happ@gmail.com>
2022-10-02 08:36:20 +0400

    feat(posts): ref type

--- routes/[id].tsx
+++ routes/[id].tsx
@@ -3,7 +3,8 @@ import { Post } from "../types.ts";
 const post: Post = {
   id: "string",
   title: "string",
-  published_at: new Date(),
+  publishedAt: new Date(),
+  snippet: "string",
   content: "string",
 };
 
@@ -11,7 +12,7 @@ export default function BlogPostPage() {
   return (
     <div class="p-4 mx-auto max-w-screen-md">
       <p class="text-grey-600 mt-12">
-        {post.published_at.toLocaleDateString()}
+        {post.publishedAt.toLocaleDateString()}
       </p>
       <h1 class="text-5xl mt-2 font-bold">{post.title}</h1>
       <div class="mt-12">{post.title}</div>
