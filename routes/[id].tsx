39b81198f59a8963bc9b22521bc2a2b4f343f6be
arcbjorn <arky.happ@gmail.com>
2022-10-02 09:32:28 +0400

    feat(BlogPostPage): add post data loading

--- routes/[id].tsx
+++ routes/[id].tsx
@@ -1,6 +1,8 @@
-import { Post } from "../types.ts";
+import { IPost } from "../types.ts";
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { loadPost } from "../api/loadPost.ts";
 
-const post: Post = {
+const post: IPost = {
   id: "string",
   title: "string",
   publishedAt: new Date(),
@@ -8,7 +10,23 @@ const post: Post = {
   content: "string",
 };
 
-export default function BlogPostPage() {
+export const handler: Handlers<IPost | false> = {
+  async GET(_req, ctx) {
+    const id = ctx.params.id;
+    const post = await loadPost(id);
+    if (!post) {
+      return ctx.render(false);
+    }
+    return ctx.render(post);
+  },
+};
+
+export default function BlogPostPage(props: PageProps) {
+  const post = props.data;
+  if (!props.data) {
+    return "No post";
+  }
+
   return (
     <div class="p-4 mx-auto max-w-screen-md">
       <p class="text-grey-600 mt-12">
