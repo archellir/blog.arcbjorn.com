debcb1ee239fd78c572579ff1057c77da79b5536
arcbjorn <arky.happ@gmail.com>
2022-10-04 05:36:46 +0400

    chore(static): add fonts

--- twind.config.ts
+++ twind.config.ts
@@ -2,4 +2,52 @@ import { Options } from "$fresh/plugins/twind.ts";
 
 export default {
   selfURL: import.meta.url,
+  theme: {
+    extend: {
+      fontFamily: {
+        "plex-mono": ["IBMPlexMono", "Courier New", "Courier", "monospace"],
+        "plex-sans": ["IBMPlexSans", "sans-serif", "sans"],
+      },
+    },
+  },
+  preflight: {
+    "@font-face": [
+      {
+        fontFamily: "IBMPlexMono",
+        src: "url(/fonts/IBMPlexMono-Regular.ttf)",
+        fontWeight: "normal",
+        fontDisplay: "swap",
+      },
+      {
+        fontFamily: "IBMPlexMono",
+        src: "url(/fonts/IBMPlexMono-Bold.ttf)",
+        fontWeight: "bold",
+        fontDisplay: "swap",
+      },
+      {
+        fontFamily: "IBMPlexMono",
+        src: "url(/fonts/IBMPlexMono-SemiBold.ttf)",
+        fontWeight: "600",
+        fontDisplay: "swap",
+      },
+      {
+        fontFamily: "IBMPlexSans",
+        src: "url(/fonts/IBMPlexSans-Regular.ttf)",
+        fontWeight: "normal",
+        fontDisplay: "swap",
+      },
+      {
+        fontFamily: "IBMPlexSans",
+        src: "url(/fonts/IBMPlexSans-Bold.ttf)",
+        fontWeight: "bold",
+        fontDisplay: "swap",
+      },
+      {
+        fontFamily: "IBMPlexSans",
+        src: "url(/fonts/IBMPlexSans-SemiBold.ttf)",
+        fontWeight: "600",
+        fontDisplay: "swap",
+      },
+    ],
+  },
 } as Options;
