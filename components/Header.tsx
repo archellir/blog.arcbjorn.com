a587f955355291a1432657d4a7c4bf85ccc12176
arcbjorn <arky.happ@gmail.com>
2022-10-04 06:04:32 +0400

    feat(components): add header

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div class="flex items-center justify-around flex-wrap p-12">
+      <div class="cursor-pointer font-bold text-xl px-7 font-plex-mono">
+        arcbjorn
+      </div>
+      <h1 class="text-xl font-plex-sans">public thoughtbook</h1>
+    </div>
+  );
+}
