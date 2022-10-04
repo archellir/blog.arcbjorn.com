843078c66499279605ff1b15a9cee6a3003e1137
arcbjorn <arky.happ@gmail.com>
2022-10-04 05:34:57 +0400

    feat (global app layout): add _app.tsx

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,14 +4,16 @@
 
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[id].tsx";
-import * as $1 from "./routes/_middleware.tsx";
-import * as $2 from "./routes/index.tsx";
+import * as $1 from "./routes/_app.tsx";
+import * as $2 from "./routes/_middleware.tsx";
+import * as $3 from "./routes/index.tsx";
 
 const manifest = {
   routes: {
     "./routes/[id].tsx": $0,
-    "./routes/_middleware.tsx": $1,
-    "./routes/index.tsx": $2,
+    "./routes/_app.tsx": $1,
+    "./routes/_middleware.tsx": $2,
+    "./routes/index.tsx": $3,
   },
   islands: {},
   baseUrl: import.meta.url,
