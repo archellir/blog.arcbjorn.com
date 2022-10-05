1cf16424fb2e74732f8e529387bf90caf4facafc
arcbjorn <arky.happ@gmail.com>
2022-10-05 04:30:47 +0400

    feat(Header): adapt small for screen

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,10 @@
 export default function Header() {
   return (
-    <div class="flex items-center justify-around gap-4 flex-wrap p-12">
+    <div class="flex items-center justify-between md:justify-evenly gap-4 flex-wrap p-12">
       <a href="/" class="cursor-pointer font-bold text-xl font-plex-mono">
         arcbjorn
       </a>
+      <div class="lg:hidden" />
       <h1 class="text-lg font-plex-sans">thoughtbook</h1>
     </div>
   );
