c4354222fc547b480cde85546f2fe8d25ecc8491
arcbjorn <arky.happ@gmail.com>
2022-10-05 04:40:59 +0400

    feat(index): polish styles

--- routes/index.tsx
+++ routes/index.tsx
@@ -20,12 +20,17 @@ function Post(props: { post: IPost; locales: IState["locales"] }) {
   const dateFmt = new Intl.DateTimeFormat("en-UK", { dateStyle: "short" });
 
   return (
-    <li class="border-t mt-8">
-      <a href={`/${post.id}`} class="p-2 flex gap-4 group">
-        <div>{dateFmt.format(post.publishedAt)}</div>
+    <li class="border-t py-4 px-2 mx-8">
+      <a
+        href={`/${post.id}`}
+        class="p-2 flex justify-start items-baseline flex-wrap md:flex-nowrap gap-y-2 gap-x-4 group"
+      >
+        <div class="font-plex-mono">{dateFmt.format(post.publishedAt)}</div>
         <div>
-          <h2 class="text-xl font-bold group-hover:underline">{post.title}</h2>
-          <p class="text-grey-600">{post.snippet}</p>
+          <h2 class="text-xl tracking-tight font-plex-mono font-semibold group-hover:underline">
+            {post.title}
+          </h2>
+          <p class="pt-1 font-plex-sans text-grey-600">{post.snippet}</p>
         </div>
       </a>
     </li>
@@ -37,7 +42,7 @@ export default function Home(props: PageProps<IData>) {
   return (
     <>
       <HeadElement
-        description="Public thoughtbook of Arcbjorn"
+        description="Thoughtbook of Arcbjorn"
         image={url.href + "logo.svg"}
         title="Arc's thoughtbook"
         url={url}
@@ -45,7 +50,7 @@ export default function Home(props: PageProps<IData>) {
 
       <div class="flex flex-col">
         <Header />
-        <div class="max-w-screen-lg self-center">
+        <div class="max-w-screen-md self-center">
           <ul>
             {data.posts.map((post) => (
               <Post post={post} locales={data.locales} />
