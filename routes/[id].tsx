3d92ba4c18765bb0bd085ae095866a4a2c7c1c90
arcbjorn <arky.happ@gmail.com>
2022-10-05 05:46:31 +0400

    feat(BlogPage): add Header & styles

--- routes/[id].tsx
+++ routes/[id].tsx
@@ -3,6 +3,7 @@ import { CSS, render } from "$gfm";
 import { IPost, IState } from "../types.ts";
 import { loadPost } from "../api/loadPost.ts";
 import { HeadElement } from "../components/HeadElement.tsx";
+import { Header } from "../components/Header.tsx";
 
 import "https://esm.sh/prismjs@1.27.0/components/prism-typescript?no-check";
 import "https://esm.sh/prismjs@1.27.0/components/prism-bash?no-check";
@@ -35,22 +36,30 @@ export default function BlogPostPage(props: PageProps) {
   }
 
   return (
-    <div class="p-4 mx-auto max-w-screen-md">
+    <>
       <HeadElement
         title={data.post.title}
         description={data.post.snippet}
         url={url}
       />
 
-      <p class="text-grey-600 mt-12 font-plex-mono">
-        {dateFmt.format(data.post.publishedAt)}
-      </p>
-      <h1 class="text-5xl mt-2 font-bold font-plex-mono">{data.post.title}</h1>
-      <style class="mt-12" dangerouslySetInnerHTML={{ __html: CSS }} />
-      <div
-        class="mt-12 !font-plex-sans markdown-body"
-        dangerouslySetInnerHTML={{ __html: html }}
-      />
-    </div>
+      <Header />
+      <div class="p-8 mx-auto max-w-screen-md">
+        <p class="text-grey-600 font-plex-mono">
+          {dateFmt.format(data.post.publishedAt)}
+        </p>
+
+        <h1 class="text-5xl py-8 font-semibold font-plex-mono">
+          {data.post.title}
+        </h1>
+
+        <style dangerouslySetInnerHTML={{ __html: CSS }} />
+
+        <div
+          class="!font-plex-sans markdown-body"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </div>
+    </>
   );
 }
