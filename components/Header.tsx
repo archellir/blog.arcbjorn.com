0aeed8aa042f0db469784178d631c132044243b5
arcbjorn <arky.happ@gmail.com>
2022-10-05 05:45:21 +0400

    ref(Header): adapt small for screen

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,9 @@
-export default function Header() {
+export function Header() {
   return (
-    <div class="flex items-center justify-between md:justify-evenly gap-4 flex-wrap p-12">
+    <div class="flex justify-between lg:justify-around gap-4 flex-wrap p-12">
       <a href="/" class="cursor-pointer font-bold text-xl font-plex-mono">
         arcbjorn
       </a>
-      <div class="lg:hidden" />
       <h1 class="text-lg font-plex-sans">thoughtbook</h1>
     </div>
   );
