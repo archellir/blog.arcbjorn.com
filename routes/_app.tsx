24d37d0783956b1438933f1e81a4e527e66a968a
arcbjorn <arky.happ@gmail.com>
2022-10-09 02:33:30 +0400

    ref(app): remove Header

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -2,17 +2,9 @@ import { FunctionalComponent } from "preact";
 import { AppProps } from "$fresh/server.ts";
 import { Head } from "$fresh/runtime.ts";
 
-import { Header } from "../components/Header.tsx";
-
 const App: FunctionalComponent<AppProps> = ({ Component }) => {
   return (
     <div class="container mx-auto h-full animate-appear">
-      <Head>
-        <link rel="stylesheet" href="/app.css" />
-      </Head>
-
-      <Header />
-
       <Component />
     </div>
   );
