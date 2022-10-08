7753d25fd849389e15812b3621130a02748e494a
arcbjorn <arky.happ@gmail.com>
2022-10-08 23:30:37 +0400

    feat(components): add Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,16 @@
+export function Footer() {
+  return (
+    <div class="mt-auto flex justify-center p-8">
+      <a
+        rel="license"
+        href="http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_GB"
+      >
+        <img
+          alt="Creative Commons Licence"
+          style="border-width:0"
+          src="https://mirrors.creativecommons.org/presskit/buttons/80x15/svg/by-nc-sa.svg"
+        />
+      </a>
+    </div>
+  );
+}
