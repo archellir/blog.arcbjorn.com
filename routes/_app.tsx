e0025e2edbee7a1ca4dec3b5bddc495fd2c5bc5d
arcbjorn <arky.happ@gmail.com>
2022-10-08 23:30:49 +0400

    feat(app): use Footer

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,10 +1,11 @@
 import { AppProps } from "$fresh/server.ts";
 import { Head } from "$fresh/runtime.ts";
 import { Header } from "../components/Header.tsx";
+import { Footer } from "../components/Footer.tsx";
 
 export default function App({ Component }: AppProps) {
   return (
-    <div class="container mx-auto h-full animate-appear">
+    <div class="container flex flex-col mx-auto h-full animate-appear">
       <Head>
         <link rel="stylesheet" href="/app.css" />
       </Head>
@@ -12,6 +13,8 @@ export default function App({ Component }: AppProps) {
       <Header />
 
       <Component />
+
+      <Footer />
     </div>
   );
 }
