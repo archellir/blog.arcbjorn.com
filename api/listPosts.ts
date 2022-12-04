20587a7d9d774a202b20c757f4aef90d30d07611
arcbjorn <arky.happ@gmail.com>
2022-12-04 20:23:07 -0300

    feat(api/listPosts): use offset, limit, tags, all

--- api/listPosts.ts
+++ api/listPosts.ts
@@ -1,16 +1,54 @@
 import { loadPost } from "./loadPost.ts";
 
-import { IPost } from "../types.ts";
+import { IListLoadParams, IPost, IPostsResponse } from "../types.ts";
 
-export async function listPosts(): Promise<IPost[]> {
+const defaultProps: IListLoadParams = {
+  limit: 5,
+  offset: 0,
+};
+
+export async function listPosts(
+  props: IListLoadParams = defaultProps,
+): Promise<IPostsResponse> {
   const promises: Promise<IPost | null>[] = [];
   for await (const entry of Deno.readDir("./data/posts")) {
     const id = entry.name.slice(0, -3);
     promises.push(loadPost(id));
   }
 
-  const posts = await Promise.all(promises) as IPost[];
-  posts.sort((a, b) => b.publishedAt.getTime() - a.publishedAt.getTime());
+  let posts = await Promise.all(promises) as IPost[];
+  posts.sort((a, b) =>
+    new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime()
+  );
 
-  return posts as IPost[];
+  const allPostsQuantity = posts.length;
+
+  // return all posts when no limit
+  if (props.limit === 0) {
+    return { posts, all: true };
+  }
+
+  const arrayOffset = props.offset > 0 ? props.offset : 0;
+  posts = posts.slice(arrayOffset, arrayOffset + props.limit);
+
+  if (props.tags?.length) {
+    posts = posts.filter(({ tags }) => {
+      let hasTag = false;
+
+      if (tags && tags.length) {
+        tags.forEach((tag) => props.tags!.includes(tag));
+        for (let i = 0; i < tags.length; i++) {
+          if (props.tags!.includes(tags[i])) {
+            hasTag = true;
+          }
+        }
+      }
+
+      return hasTag;
+    });
+  }
+
+  const all = allPostsQuantity === posts.length;
+
+  return { posts, all };
 }
