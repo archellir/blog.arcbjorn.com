0a8c878c3f13e328f2d8704778c829afe1ad3745
arcbjorn <arky.happ@gmail.com>
2022-12-04 20:37:49 -0300

    feat(routes): add `posts` API

---
+++ routes/posts.ts
@@ -0,0 +1,20 @@
+import { HandlerContext, Handlers } from "$fresh/server.ts";
+import { listPosts } from "../api/listPosts.ts";
+import { IPostsResponse } from "../types.ts";
+import { getListLoadParams } from "../utils/getListParams.ts";
+
+export const handler: Handlers = {
+  async GET(req: Request, _ctx: HandlerContext) {
+    const listParams = getListLoadParams(req.url);
+
+    const postsData: IPostsResponse = await listPosts(listParams);
+    const headers = new Headers({
+      "Content-Type": "application/json",
+    });
+
+    return new Response(JSON.stringify(postsData), {
+      status: 200,
+      headers,
+    });
+  },
+};
