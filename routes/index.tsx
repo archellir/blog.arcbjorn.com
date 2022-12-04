c70208c598f2157792b7fe52b508e7eff8bc202c
arcbjorn <arky.happ@gmail.com>
2022-12-04 20:39:48 -0300

    ref(routes/index): use `PostsList`

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,23 +1,23 @@
 import { FunctionalComponent } from "preact";
 import { Handlers, PageProps } from "$fresh/server.ts";
 
-import { IPost, IState } from "../types.ts";
+import HeadElement from "../components/HeadElement.tsx";
+import Header from "../components/Header.tsx";
+import Footer from "../components/Footer.tsx";
+import Introduction from "../components/Introduction.tsx";
+import PostsList from "../islands/PostsList.tsx";
+
+import { IHomePageData, IPostsResponse, IState } from "../types.ts";
 import { listPosts } from "../api/listPosts.ts";
 
-import { HeadElement } from "../components/HeadElement.tsx";
-import { Header } from "../components/Header.tsx";
-import { Post } from "../components/Post.tsx";
-import { Footer } from "../components/Footer.tsx";
-import Introduction from "../components/Introduction.tsx";
-
-interface IHomePageData extends IState {
-  posts: IPost[];
-}
+import { getFirstLoadListParams } from "../utils/getListParams.ts";
 
 export const handler: Handlers<IHomePageData, IState> = {
-  async GET(_req, ctx) {
-    const posts = await listPosts();
-    return ctx.render({ ...ctx.state, posts });
+  async GET(req, ctx) {
+    const listParams = getFirstLoadListParams(req.url);
+    const data: IPostsResponse = await listPosts(listParams);
+
+    return ctx.render({ ...ctx.state, posts: data.posts });
   },
 };
 
@@ -37,13 +37,7 @@ const Home: FunctionalComponent<PageProps<IHomePageData>> = (props) => {
 
         <Introduction />
 
-        <div class="max-w-screen-lg self-center flex-grow pt-8 sm:pt-16">
-          <ul>
-            {data.posts.map((post) => (
-              <Post post={post} locales={data.locales} />
-            ))}
-          </ul>
-        </div>
+        <PostsList posts={data.posts} locales={data.locales} />
 
         <Footer classes="mt-auto" />
       </div>
