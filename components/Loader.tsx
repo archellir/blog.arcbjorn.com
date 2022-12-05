58a7ef6d09adabee1073b15e6725f2cad733ca19
arcbjorn <arky.happ@gmail.com>
2022-12-05 02:26:24 -0300

    ref(components/Loader): padding

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -2,7 +2,7 @@ import { FunctionalComponent } from "preact";
 
 const Loader: FunctionalComponent = () => {
   return (
-    <div class="self-center pt-4 pb-12">
+    <div class="self-center pt-8 pb-12">
       <div class="dot-wave">
         <div class="dot-wave__dot"></div>
         <div class="dot-wave__dot"></div>
