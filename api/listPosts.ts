ef8d56a67437939f46e705b62bdedbf0f55c9acf
arcbjorn <arky.happ@gmail.com>
2022-12-04 22:40:24 -0300

    fix(api/listPosts): tags filtering

--- api/listPosts.ts
+++ api/listPosts.ts
@@ -21,22 +21,14 @@ export async function listPosts(
     new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime()
   );
 
-  const allPostsQuantity = posts.length;
+  let allPostsQuantity = posts.length;
 
-  // return all posts when no limit
-  if (props.limit === 0) {
-    return { posts, all: allPostsQuantity };
-  }
-
-  const arrayOffset = props.offset > 0 ? props.offset : 0;
-  posts = posts.slice(arrayOffset, arrayOffset + props.limit);
-
+  // filter by tags first to record the quantity
   if (props.tags?.length) {
     posts = posts.filter(({ tags }) => {
       let hasTag = false;
 
       if (tags && tags.length) {
-        tags.forEach((tag) => props.tags!.includes(tag));
         for (let i = 0; i < tags.length; i++) {
           if (props.tags!.includes(tags[i])) {
             hasTag = true;
@@ -46,7 +38,18 @@ export async function listPosts(
 
       return hasTag;
     });
+
+    // all posts quantity by tags
+    allPostsQuantity = posts.length;
   }
+
+  // return all posts when no limit
+  if (props.limit === 0) {
+    return { posts, all: allPostsQuantity };
+  }
+
+  const arrayOffset = props.offset > 0 ? props.offset + 1 : 0;
+  posts = posts.slice(arrayOffset, arrayOffset + props.limit);
 
   return { posts, all: allPostsQuantity };
 }
