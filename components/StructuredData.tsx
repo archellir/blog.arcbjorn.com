b02eb2fe7dc4ee9fbe7ccdc0ab415ff2b110155f
arcbjorn <arky.happ@gmail.com>
2025-08-07 22:39:27 -0300

    feat: add reusable StructuredData component

---
+++ components/StructuredData.tsx
@@ -0,0 +1,23 @@
+import { FunctionalComponent } from "preact";
+
+export interface StructuredDataProps {
+  type: "BlogPosting" | "WebPage" | "Organization";
+  data: Record<string, any>;
+}
+
+const StructuredData: FunctionalComponent<StructuredDataProps> = ({ type, data }) => {
+  const structuredData = {
+    "@context": "https://schema.org",
+    "@type": type,
+    ...data
+  };
+
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
+    />
+  );
+};
+
+export default StructuredData;
