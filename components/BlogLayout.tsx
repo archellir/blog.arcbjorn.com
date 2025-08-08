1be79ce43cc41ffec17bc2eebcfad6bd08472e50
arcbjorn <arky.happ@gmail.com>
2025-08-07 22:39:40 -0300

    feat: add BlogLayout component for common page structure

---
+++ components/BlogLayout.tsx
@@ -0,0 +1,43 @@
+import { FunctionalComponent, ComponentChildren } from "preact";
+import { asset } from "$fresh/runtime.ts";
+import { CSS, KATEX_CSS } from "$gfm";
+
+import Header from "./Header.tsx";
+import Footer from "./Footer.tsx";
+import MermaidScript from "./MermaidScript.tsx";
+
+interface BlogLayoutProps {
+  children: ComponentChildren;
+  includeCodeHighlighting?: boolean;
+}
+
+const BlogLayout: FunctionalComponent<BlogLayoutProps> = ({ 
+  children, 
+  includeCodeHighlighting = false 
+}) => {
+  return (
+    <>
+      <style dangerouslySetInnerHTML={{ __html: CSS }} />
+      <style dangerouslySetInnerHTML={{ __html: KATEX_CSS }} />
+      
+      {includeCodeHighlighting && (
+        <>
+          <link rel="stylesheet" href={asset("/gruvbox-theme.css")} />
+          <link rel="stylesheet" href={asset("/mermaid-theme.css")} />
+          <MermaidScript />
+          <script src={asset("/blog-post.js")}></script>
+        </>
+      )}
+
+      <Header />
+      
+      <main>
+        {children}
+      </main>
+
+      <Footer />
+    </>
+  );
+};
+
+export default BlogLayout;
