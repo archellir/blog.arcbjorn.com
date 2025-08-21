ee84120a93b4da9de038b5cbd4b0e490f36b48c5
arcbjorn <arky.happ@gmail.com>
2025-08-20 21:25:58 -0300

    fix(posts): list layout

--- components/Post.tsx
+++ components/Post.tsx
@@ -28,7 +28,7 @@ const Post: FunctionalComponent<IPostProps> = memo(({ post, measureRef }) => {
           <div class="flex flex-wrap gap-2 justify-between items-start">
             <time class="font-plex-mono shrink-0">{localizedDate}</time>
             {post.tags && post.tags.length > 0 && (
-              <div class="flex flex-wrap gap-2 justify-end">
+              <div class="flex flex-wrap gap-2 justify-end md:hidden lg:flex">
                 {post.tags.map((tag) => (
                   <span class="bg-gray-300 text-center text-black font-plex-mono text-sm px-2 py-1 rounded-full">
                     {tag}
@@ -45,6 +45,15 @@ const Post: FunctionalComponent<IPostProps> = memo(({ post, measureRef }) => {
           <p class="pt-1 font-plex-sans text-grey-600 hidden sm:block">
             {post.snippet}
           </p>
+          {post.tags && post.tags.length > 0 && (
+            <div class="hidden md:flex lg:hidden flex-wrap gap-2 pt-2">
+              {post.tags.map((tag) => (
+                <span class="bg-gray-300 text-center text-black font-plex-mono text-sm px-2 py-1 rounded-full">
+                  {tag}
+                </span>
+              ))}
+            </div>
+          )}
         </div>
       </a>
     </li>
