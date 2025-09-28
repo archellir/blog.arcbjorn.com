bd29a653c09dded98dae5ef909d400801e431a31
arcbjorn <arky.happ@gmail.com>
2025-09-28 03:19:27 -0300

    fix(layout): remove unused state param

--- routes/_layout.tsx
+++ routes/_layout.tsx
@@ -3,7 +3,7 @@ import { define } from "../fresh.ts";
 import Header from "../components/Header.tsx";
 import Footer from "../components/Footer.tsx";
 
-export default define.layout(({ Component, state }) => {
+export default define.layout(({ Component }) => {
   return (
     <div class="flex flex-col h-full">
       <Header />
