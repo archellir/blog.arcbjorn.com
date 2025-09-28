1061a17c4ef3dfc277c08270a930dd78c6d69f42
arcbjorn <arky.happ@gmail.com>
2025-09-28 01:29:39 -0300

    feat(fonts): add manrope/inter typography

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -9,7 +9,7 @@ export default define.page(function App({ Component }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro:ital,wght@0,400;0,600;0,700;1,400;1,600&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700&family=Inter:wght@400;500;600&family=Fira+Code:wght@400;500&display=swap"
           rel="stylesheet"
         />
         <link rel="icon" href="/favicon.ico" sizes="32x32" />
