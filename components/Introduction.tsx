004dee948cfc1a14996e778a9695c58f47d6b6d0
arcbjorn <arky.happ@gmail.com>
2025-09-28 15:15:05 -0300

    style(intro): apply wavy link to handle

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -5,13 +5,12 @@ const Introduction: FunctionalComponent = () => {
     <div class="self-center text-center p-8 max-w-screen-lg text-sm sm:text-base lg:border-x border-t border-dashed border-gray-400">
       <div class="p-1">
         My name is Oleg{" "}
-        <span class="text-green-400">[al'eg]</span>. Online handle is{" "}
-        <a
+        <span class="text-green-400">[al'eg]</span>. Online handle is @<a
           target="_blank"
           href="https://arcbjorn.com"
-          class="underline"
+          class="wavy-link"
         >
-          @arcbjorn
+          arcbjorn
         </a>.
       </div>
       <div class="p-1">
